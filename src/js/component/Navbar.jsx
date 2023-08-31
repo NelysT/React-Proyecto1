bbbe93a21b0f10f6a6fbf3771f8d8d96732111fd
NelysT <139475672+NelysT@users.noreply.github.com>
2023-08-31 08:22:09 +0000

    Cambios para hacerlo responsive

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,21 +2,32 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className="navbar navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Start Bootstrap</a>
-                    <div className="d-flex justify-content-end">
-                        <a className="navbar-brand" href="#">Home</a>
-                        <a className="navbar-brand" href="#">About</a>
-                        <a className="navbar-brand" href="#">Services</a>
-                        <a className="navbar-brand" href="#">Contact</a>
-                    </div>
-                </div>
-            </nav>
-        </div>
-
+        <nav className="navbar navbar-expand-lg bg-dark">
+          <div className="container">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav justify-content-end">
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Services</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
     );
 };
+
 
 export default Navbar;
